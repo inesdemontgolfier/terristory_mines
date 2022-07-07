daa2a4899a012f063805c18219630e1c3f678aa2
Philibert Salmon <philibert.salmon@mines-paristech.fr>
2022-07-07 17:34:49 +0200

    "correction dates partout"

--- indicateurs_v2.py
+++ indicateurs_v2.py
@@ -36,12 +36,9 @@ conn.close()
 df = pd.DataFrame(raw, columns=["id_indicateur", "nom", "ui_theme", "id_utilisateur", "date", "region", "provenance"])
 
 # On corrige les données (cf `irrégularités.py`)
+df = correction_dates(df, "id_indicateur")
 df = correction_themes(df)
-print(len(df))
 df = correction_noms(df)
-df = correction_dates(df)
-print(len(df))
-
 
 def fréquences_indic_majoritaires(data, p, regions, titre_fichier="figures/titre_par_defaut.png", titre_figure="titre_par_défaut"):
     """Renvoie les indicateurs consultés à plus de 100*p pourcents.
